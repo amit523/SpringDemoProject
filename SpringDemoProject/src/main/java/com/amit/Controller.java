2ecd54b2ddb678c93f03f3d3b905af892b60d599
Rupal <amitjangra1864@gmail.com>
2026-01-28 18:37:29 +0530

    first

---
+++ SpringDemoProject/src/main/java/com/amit/Controller.java
@@ -0,0 +1,20 @@
+package com.amit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@GetMapping("/")
+	public String home() {
+		return new String("*** This is Amit Jangra ***");
+	}
+	
+	
+	
+
+	
+	 
+	
+}
---
+++ SpringDemoProject/src/main/java/com/amit/ProjectRunner.java
@@ -0,0 +1,19 @@
+package com.amit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectRunner implements CommandLineRunner{
+	
+	@Autowired
+	private Controller controller = new Controller();
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		controller.home();
+	}
+
+}
