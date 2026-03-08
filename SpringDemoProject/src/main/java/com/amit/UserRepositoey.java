bca3f3ad248427af3cc5083efe679e90248ace9d
amit523 <45826875+amit523@users.noreply.github.com>
2026-03-08 12:30:05 +0530

    Update UserRepositoey.java

--- SpringDemoProject/src/main/java/com/amit/UserRepositoey.java
+++ SpringDemoProject/src/main/java/com/amit/UserRepositoey.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //@Repository
-public interface UserRepositoey extends JpaRepository<User, Long> {
+public interface UserRepositoey  {
+  //extends JpaRepository<User, Long>
 }
+
 
