55e6fc282982a562283bb7f0abece4d2834895cf
amit523 <45826875+amit523@users.noreply.github.com>
2026-03-08 12:37:44 +0530

    Update SpringDemoProjectApplication.java

--- SpringDemoProject/src/main/java/com/amit/SpringDemoProjectApplication.java
+++ SpringDemoProject/src/main/java/com/amit/SpringDemoProjectApplication.java
@@ -2,8 +2,9 @@ package com.amit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}
 public class SpringDemoProjectApplication {
 
 	public static void main(String[] args) {
